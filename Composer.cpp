b49b7a03bf9347b120799d98bd7d0561c00f3f16
apselon <ildarovadam@gmail.com>
2020-05-13 19:53:59 +0300

    [2] Added slow version

--- Composer.cpp
+++ Composer.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <cstdlib>
 #include <immintrin.h>
 
@@ -21,14 +22,21 @@ void compose(char* front, size_t f_width, size_t f_height, char* back, size_t b_
 	for (size_t y = 0; y < f_height; ++y){
 		for (size_t x = 0; x < f_width; x += 4){
 
-			__m128i bp   = _mm_loadu_si128(reinterpret_cast<__m128i*>(back  + x * 4));
-			__m128i fp   = _mm_loadu_si128(reinterpret_cast<__m128i*>(front + x * 4));
+			__m128i bp   = _mm_lddqu_si128(reinterpret_cast<__m128i*>(back  + x * 4));
+			__m128i fp   = _mm_lddqu_si128(reinterpret_cast<__m128i*>(front + x * 4));
 
-			__m128i bp_h = (__m128i)_mm_movehl_ps((__m128)zeroes, (__m128)bp); 
-			__m128i bp_l = (__m128i)_mm_movelh_ps((__m128)bp, (__m128)zeroes); 
+			__m128i bp_h = reinterpret_cast<__m128i>(_mm_movehl_ps(reinterpret_cast<__m128>(zeroes),
+			                                                       reinterpret_cast<__m128>(bp    )));
 
-			__m128i fp_h = (__m128i)_mm_movehl_ps((__m128)zeroes, (__m128)fp); 
-			__m128i fp_l = (__m128i)_mm_movelh_ps((__m128)fp, (__m128)zeroes); 
+			__m128i bp_l = reinterpret_cast<__m128i>(_mm_movelh_ps(reinterpret_cast<__m128>(bp    ),
+			                                                       reinterpret_cast<__m128>(zeroes)));
+
+
+			__m128i fp_h = reinterpret_cast<__m128i>(_mm_movehl_ps(reinterpret_cast<__m128>(zeroes),
+			                                                       reinterpret_cast<__m128>(fp    )));
+
+			__m128i fp_l = reinterpret_cast<__m128i>(_mm_movelh_ps(reinterpret_cast<__m128>(fp    ),
+			                                                       reinterpret_cast<__m128>(zeroes)));
 
 			__m128i bp_h_ext = _mm_cvtepu8_epi16(bp_h);
 			__m128i bp_l_ext = _mm_cvtepu8_epi16(bp_l);
@@ -36,17 +44,17 @@ void compose(char* front, size_t f_width, size_t f_height, char* back, size_t b_
 			__m128i fp_h_ext = _mm_cvtepu8_epi16(fp_h);
 			__m128i fp_l_ext = _mm_cvtepu8_epi16(fp_l);
 
-			__m128i fpa_h  = _mm_shuffle_epi8(fp_h_ext, alpha_mask);
-			__m128i fpa_l  = _mm_shuffle_epi8(fp_l_ext, alpha_mask);
+			__m128i fpa_h    = _mm_shuffle_epi8(fp_h_ext, alpha_mask);
+			__m128i fpa_l    = _mm_shuffle_epi8(fp_l_ext, alpha_mask);
 			
-			__m128i nbp_h = _mm_mullo_epi16(bp_h_ext, _mm_sub_epi16(OxFF_mask, fpa_h));
-			__m128i nbp_l = _mm_mullo_epi16(bp_l_ext, _mm_sub_epi16(OxFF_mask, fpa_l));
+			__m128i nbp_h    = _mm_mullo_epi16(bp_h_ext, _mm_sub_epi16(OxFF_mask, fpa_h));
+			__m128i nbp_l    = _mm_mullo_epi16(bp_l_ext, _mm_sub_epi16(OxFF_mask, fpa_l));
 
-			__m128i nfp_h = _mm_mullo_epi16(fp_h_ext, fpa_h);
-			__m128i nfp_l = _mm_mullo_epi16(fp_l_ext, fpa_l);
+			__m128i nfp_h    = _mm_mullo_epi16(fp_h_ext, fpa_h);
+			__m128i nfp_l    = _mm_mullo_epi16(fp_l_ext, fpa_l);
 
-			__m128i res_h  = _mm_srli_epi16(_mm_add_epi16(nbp_h, nfp_h), 8);
-			__m128i res_l  = _mm_srli_epi16(_mm_add_epi16(nbp_l, nfp_l), 8);
+			__m128i res_h    = _mm_srli_epi16(_mm_add_epi16(nbp_h, nfp_h), 8);
+			__m128i res_l    = _mm_srli_epi16(_mm_add_epi16(nbp_l, nfp_l), 8);
 
 			res_h = _mm_shuffle_epi8(res_h, extract_mask_h);
 			res_l = _mm_shuffle_epi8(res_l, extract_mask_l);
@@ -58,3 +66,23 @@ void compose(char* front, size_t f_width, size_t f_height, char* back, size_t b_
 		back  += 4 * b_width;
 	}
 }
+
+void slow_compose(char* front, size_t f_width, size_t f_height, char* back, size_t b_width, size_t b_height){
+
+	for (size_t y = 0; y < f_height; ++y){
+		for (size_t x = 0; x < f_width; x += 1){
+
+			uint8_t alpha = *(uint8_t*)(front + 4 * x + 3);
+			uint8_t inv_alpha = 255 - alpha;
+
+			back[4 * x]     = (uint8_t)((alpha * front[4 * x]     + inv_alpha * back[4 * x])     >> 8);
+			back[4 * x + 1] = (uint8_t)((alpha * front[4 * x + 1] + inv_alpha * back[4 * x + 1]) >> 8);
+			back[4 * x + 2] = (uint8_t)((alpha * front[4 * x + 2] + inv_alpha * back[4 * x + 2]) >> 8);
+
+		}
+
+	front += 4 * f_width;
+	back  += 4 * b_width;
+	}
+}
+
